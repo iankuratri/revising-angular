e5ae1e4883151a2e45635fb78c3c532d07f42a91
Ankur Atri <ankuratri.in@gmail.com>
2021-10-06 21:22:16 +0530

    ngForm, ngModelGroup and ngSubmit example added

--- template-driven-forms/src/app/contact-form/contact-form.component.ts
+++ template-driven-forms/src/app/contact-form/contact-form.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  submit(form: any) {
+    console.log(form.value);
   }
-
 }
