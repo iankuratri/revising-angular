d55309894bee1ff4d6e8c9b323ff63bccf2f3a36
Ankur Atri <ankuratri.in@gmail.com>
2021-10-10 21:04:28 +0530

    npm install --save web-animations-js

--- animations/src/app/app.module.ts
+++ animations/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { TodosComponent } from './todos/todos.component';
 import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent,
-    ZippyComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodosComponent, ZippyComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
