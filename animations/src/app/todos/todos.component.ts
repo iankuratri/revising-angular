4192267e3d46281b55c8b48bfd9bf5fed30f10fc
Ankur Atri <ankuratri.in@gmail.com>
2021-10-10 21:22:42 +0530

    tansition alias used instead of state change expression

--- animations/src/app/todos/todos.component.ts
+++ animations/src/app/todos/todos.component.ts
@@ -14,8 +14,7 @@ import { Component } from '@angular/core';
   animations: [
     trigger('fade', [
       state('void', style({ opacity: 0 })),
-      transition('void => *', [animate(2000)]),
-      transition('* => void', [animate(2000)]),
+      transition(':enter, :leave', [animate(2000)]),
     ]),
   ],
 })
