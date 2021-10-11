e8f69a6b771209b1cd32029c0651fa50b101a915
Ankur Atri <ankuratri.in@gmail.com>
2021-10-11 18:41:07 +0530

    easing animations

--- animations/src/app/animations.ts
+++ animations/src/app/animations.ts
@@ -11,7 +11,18 @@ export let fade = trigger('fade', [
   transition(':enter, :leave', [animate(2000)]),
 ]);
 
+// export let slide = trigger('slide', [
+//   state('void', style({ opacity: 0, transform: 'translateX(-20px)' })),
+//   transition(':enter, :leave', [animate(500)]),
+// ]);
+
 export let slide = trigger('slide', [
-  state('void', style({ opacity: 0, transform: 'translateX(-20px)' })),
-  transition(':enter, :leave', [animate(500)]),
+  transition(':enter', [
+    style({ transform: 'translateX(-10px)' }),
+    animate(500),
+  ]),
+
+  transition(':leave', [
+    animate('0.5s ease-in', style({ transform: 'translateX(-100%)' })),
+  ]),
 ]);
