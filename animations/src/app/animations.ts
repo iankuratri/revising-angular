83825ed45bca2f9906f62fee2ce5912687007d29
Ankur Atri <ankuratri.in@gmail.com>
2021-10-11 20:51:32 +0530

    using keyframes instead style fn for bounce out effect

--- animations/src/app/animations.ts
+++ animations/src/app/animations.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -23,6 +24,13 @@ export let slide = trigger('slide', [
   ]),
 
   transition(':leave', [
-    animate('0.5s ease-in', style({ transform: 'translateX(-100%)' })),
+    animate(
+      '0.5s ease-out',
+      // style({ transform: 'translateX(-100%)' })
+      keyframes([
+        style({ offset: 0.2, opacity: 1, transform: 'translateX(20px)' }),
+        style({ offset: 1, opacity: 0, transform: 'translateX(-100%)' }),
+      ])
+    ),
   ]),
 ]);
